692e4d7e60755c1abc51bb8f541fe53b11c69984
Shumani Simeli <shumanisimeli>
2024-04-27 00:22:49 +0200

    updated api

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ from api.v1.views import app_views
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix="/app")
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,15 +2,8 @@
 """
 """
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import jsonify
 
-
-app = Flask(__name__)
-
-@app.route('/status')
+@app_views.route('/status')
 def status():
     return jsonify({"status": "OK"})
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
