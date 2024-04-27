489650691f338c347f62e085c3a0bcee0318f66a
sanieeme <shoneesani@gmail.com>
2024-04-27 07:39:05 +0000

    task 4 to be updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,21 @@
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
+
+storage = storage()
 
 
 @app_views.route('/status')
 def status():
     return jsonify({"status": "OK"})
+
+
+@app.route('/api/v1/stats', methods='GET')
+def stats():
+    stat = {}
+    for clss, cls in storage.classes.items():
+        count = storage.count(cls)
+        stats[clss] = count
+    return jsonify(stat)
