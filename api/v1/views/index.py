21c2e214e8f3cec3f2f1e317e099339870b195ba
sanieeme <shoneesani@gmail.com>
2024-04-27 07:14:09 +0000

    pull

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,12 +5,6 @@ from api.v1.views import app_views
 from flask import Flask, jsonify
 
 
-app = Flask(__name__)
-
 @app.route('/status')
 def status():
-    return jsonify({"status": "OK"})
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+    return jsonify({"status": "OK"}):wq
