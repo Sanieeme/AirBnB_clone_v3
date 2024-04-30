8ab88cfccff9a18fa6597ea7d0d0e6ed90bf264c
sanieeme <shoneesani@gmail.com>
2024-04-30 10:57:28 +0000

    some stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,19 @@ from flask import jsonify
 @app_views.route('/status')
 def status():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def stats():
+    stat = {}
+    classes = {'amenities': Amenity,
+               'cities': City,
+               'places': Place,
+               'reviews': Review,
+               'states': State,
+               'users': User
+             }
+
+    for clss, cls in classes.items():
+        count = storage.count(cls)
+        stat[clss] = count
+    return jsonify(stat)
