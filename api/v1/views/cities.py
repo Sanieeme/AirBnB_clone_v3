e5cafa56e2a79b8b2429dd4a974a5b3fabba7c54
Shumani Simeli <shumanisimeli>
2024-05-01 14:03:35 +0200

    updated __init__.py and created cities.py

---
+++ api/v1/views/cities.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""cities.py"""
+
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+from models import storage
+from models.city import City
+from models.state import State
+
+
+@app_views.route('/states/<string:state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
+def get_cities(state_id):
+    """get city information for all cities in a specified state"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    return jsonify([city.to_dict() for city in state.cities])
+
+
+@app_views.route('/cities/<string:city_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_city(city_id):
+    """get city information for specified city"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    return jsonify(city.to_dict())
+
